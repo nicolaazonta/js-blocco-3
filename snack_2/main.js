be33fc4e5ca6f1b651db086fccd6c84bd7cd88d6
nicolaazonta <nicolazonta114@gmail.com>
2023-03-23 12:24:07 +0100

    complete capitalized exercise

---
+++ snack_2/main.js
@@ -0,0 +1,20 @@
+/* A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+const bikes = ['giant','faNTIC','BIANCHI','BMC','galetti'];
+
+bikes.forEach(bike => {
+   // console.log((bike.toLowerCase()));
+
+    const wordCapitalized = bike.toLowerCase()
+
+    const firstLetter = wordCapitalized.charAt(0)
+
+    const firstLetterCap = firstLetter.toUpperCase()
+
+    const remainingLetters = wordCapitalized.slice(1)
+
+    const word = firstLetterCap + remainingLetters   
+    console.log(word);
+});

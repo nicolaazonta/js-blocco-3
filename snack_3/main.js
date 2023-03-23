a100648011fceb65cb40ef1e9079896b7f116862
nicolaazonta <nicolazonta114@gmail.com>
2023-03-23 12:59:17 +0100

    complete mammiferi array

---
+++ snack_3/main.js
@@ -0,0 +1,47 @@
+/* Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi. */
+
+const animals = [
+    {
+        name: 'shark',
+        family: 'coi tanti denti',
+        category: 'pesce'
+    },
+
+    {
+        name: 'wolf',
+        family: 'mmmmmmmmmmm',
+        category: 'mammifero'
+    },
+
+    {
+        name: 'bear',
+        family: 'brutto',
+        category: 'mammifero'
+    },
+
+    {
+        name: 'goose',
+        family: 'bello',
+        category: 'mammifero'
+    }
+];
+
+
+const mammiferi = animals.filter (animal => {
+    if (animal.category === 'mammifero') {
+        return true;
+    }
+})
+
+console.log(mammiferi);
+
+
+
